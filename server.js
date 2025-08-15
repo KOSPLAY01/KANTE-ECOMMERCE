7a5261b7d0f9c027bd9ff2995b178ac0def0fc69
Abednego Alex <abednegoalex1@gmail.com>
2025-08-15 10:38:03 +0100

    added fix for github action

--- server.js
+++ server.js
@@ -84,12 +84,6 @@ app.get('/', (req, res) => {
   res.send('WELCOME TO ECOMMERCE PERFUME API');
 });
 
-// ping 
-app.get('/ping', (req, res) => {
-  console.log(`Ping from cron-job.org at ${new Date().toISOString()}`);
-  res.sendStatus(200); 
-});
-
 
 // User Management 
 
